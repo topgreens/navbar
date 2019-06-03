150b6ba73e8821020fb377fb504353ad0fd55a94
topgreens <topgreens@mail.com>
2019-06-02 19:12:08 -0500

    navbar using jquery libery

---
+++ script/menu.js
@@ -0,0 +1,12 @@
+(function ($) {
+  $(document).ready(function () {
+    $("button").click(function () {
+      if ($("button").text() == "☰") {
+        $("button").text("🞬");
+      } else {
+        $("button").text("☰");
+      }
+      $("li").toggle("slow");
+    });
+  });
+});
